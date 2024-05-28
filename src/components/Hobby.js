44144b883f494482f79e541c8da8cc4a449b9eea
JaredKonop <61473095+JaredKonop@users.noreply.github.com>
2024-05-28 17:38:03 +0000

    Added Up to movies

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -3,6 +3,11 @@ import "../styles/hobby.css";
 import JP from "../Assets/Joker/Jonaquin_Phoneix.png";
 import Person from "../Assets/Joker/Joker_person_w_legs.png";
 import Joker from "../Assets/Joker/Joker_text.png";
+import kid from "../Assets/Up/Kid.png";
+import house from "../Assets/Up/House.png";
+import old_guy from "../Assets/Up/Old guy.png";
+import dog from "../Assets/Up/Dog.png";
+import up from "../Assets/Up/UP.png";
 import Book from "../components/Book";
 
 const Hobby = () => {
@@ -40,21 +45,14 @@ const Hobby = () => {
               alt="joker Front image"
             />
           </article>
-          <article id="joker_poster">
-            <header id="joker_poster__header">
-              <img src={Person} id="joker_poster__header" />
+          <article id="up_poster">
+            <header id="up_poster__header">
+              <img src={house} id="up_poster__header" />
             </header>
-            <img
-              id="joker_poster__front_img"
-              src={JP}
-              alt="joker Front image"
-            />
-
-            <img
-              id="joker_poster__front_image"
-              src={Joker}
-              alt="joker Front image"
-            />
+            <img id="kid" src={kid} alt="kid" />
+            <img id="up" src={up} alt="UP" />
+            <img id="dog" src={dog} alt="dog" />
+            <img id="old_guy" src={old_guy} alt="old_guy" />
           </article>
         </div>
       </div>
